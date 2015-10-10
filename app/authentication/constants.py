42c7a4a3d92a7c2679dcd39e92c3ee8ba53def84
sergiolex <sergio.cardenas@ucsp.edu.pe>
2015-10-09 21:23:07 -0500

    Revert "#3 - #9 - #11 - #12 issues - Signin arreglado y Permisos de Usuarios"

--- app/authentication/constants.py
+++
@@ -1,7 +0,0 @@
-#constants.py contain all constants need to the project
-
-
-#User role type - permisos de usuario
-ReadRole    = 1
-CommentRole = 1<<1
-WriteRole   = 1<<2
--- app/authentication/controllers.py
+++ app/authentication/controllers.py
@@ -11,26 +11,21 @@ from flask import (
 from werkzeug import check_password_hash, generate_password_hash
 
 from app import db
-from app.authentication.constants import ReadRole, CommentRole, WriteRole
 from app.authentication.forms import LoginForm, SignupForm
 from app.authentication.models import User
 
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
-@mod_auth.route('/signout')
-def signout():
-    session.clear()
-    return redirect(url_for('auth.signin'))
-
 @mod_auth.route('/profile')
 def profile():
 
-    if 'token' not in session:
-        return redirect(url_for('auth.signin'))
+    if 'email' not in session:
+        return redirect(url_for('signin'))
 
-    user = User.verify_token(session['token'])
+    user = User.query.filter_by(email = session['email']).first()
+
     if user is None:
-        return redirect(url_for('auth.signin'))
+        return redirect(url_for('signin'))
     else:
         return render_template('authentication/profile.html')
 
@@ -39,42 +34,35 @@ def signup():
 
     form = SignupForm()
 
-    if ('token' in session) and (User.verify_token(session['token'])):
-        return redirect(url_for('auth.profile'))
+    if 'email' is session:
+        return redirect(url_for('profile'))
     
     if request.method == 'POST':
         if form.validate() == False:
             return render_template("authentication/signup.html", form=form)
         else:
-            new_user = User(form.username.data, form.email.data,\
-             form.password.data,ReadRole+CommentRole+WriteRole,1)
+            new_user = User(form.username.data, form.email.data, form.password.data)
             db.session.add(new_user)
             db.session.commit()
+
             session['email'] = new_user.email
-        return redirect(url_for('auth.profile'))
+            return "Not found"
         
     elif request.method == 'GET':
         return render_template("authentication/signup.html", form=form)
 
+
 @mod_auth.route('/signin/', methods=['GET', 'POST'])
 def signin():
 
-    if 'token' in session:
-        user = User.verify_token(session['token'])
-        if user:
-            return redirect(url_for('auth.profile'))
     form = LoginForm(request.form)
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if(user is None):
-            flash('invalid email', 'error-message')
-        elif (user.check_password(form.password.data) == True):
+        if user and check_password_hash(user.password, form.password.data):
             session['user_id'] = user.id
-            session['token'] = user.generate_token()
-            session['email'] = user.email
-            return redirect(url_for('auth.profile'))
-        else:
-            flash('invalid password', 'error-message')
+            flash('Welcome %s' % user.name)
+            return redirect(url_for('auth.home'))
 
+        flash('Wrong email or password', 'error-message')
 
     return render_template("authentication/signin.html", form=form)
